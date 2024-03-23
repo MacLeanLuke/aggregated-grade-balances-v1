46b73959bb14f66eae12562760fb26fc98f5d025
Luke MacLean <40969439+MacLeanLuke@users.noreply.github.com>
2024-03-23 16:54:13 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>HELLO WORLD</p>
+        <p>CHARTS AND GRAPHS</p>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <table>
+          
+        </table>
+
+      </div>
+    );
+  }
+}
+
+export default App;
